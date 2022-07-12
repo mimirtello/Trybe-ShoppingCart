4edf148ee460c4bc5ad3b2fce8f24ff56e93b769
Yasmin Reis Tello <mimirt08@gmail.com>
2022-07-12 09:17:53 -0300

    Requisito 3

--- helpers/fetchItem.js
+++ helpers/fetchItem.js
@@ -1,6 +1,10 @@
-const fetchItem = () => {
-  // seu código aqui.
+const fetchItem = async (item) => {
+  const url = `https://api.mercadolibre.com/items/${item}`;
+  const result = await fetch(url);
+  const data = await result.json();
+  return data;
 };
+fetchItem('MLB1615760527');
 
 if (typeof module !== 'undefined') {
   module.exports = {
